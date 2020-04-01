05b1936b177b5c8d578136c871be146f8249a367
jenjnif <jenjnif@hotmail.com>
2020-04-01 09:39:26 +0100

    Initial commit

---
+++ broken_calculator.py
@@ -0,0 +1,24 @@
+'''
+This week's question:
+You're given a broken calculator that has a number showing on its display.
+You can only perform two operations:
+Double the number on the display
+Subtract 1 from the number on the display
+
+Write a function that takes in the number X (the initial number on
+the display) and Y (the result that you want) and returns the
+minimum number of operations needed to display Y.
+
+Example:
+
+> brokenCalc(3, 10)
+> 3                // 3 -> 6 -> 5 -> 10
+'''
+
+
+def broken_calc(X, Y):
+    pass
+
+
+def test_answer():
+    pass
