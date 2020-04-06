21fa451f69d0a6f0d95b348eb64e4e18e99e1835
jenjnif <jenjnif@hotmail.com>
2020-04-06 12:46:40 +0100

    Write long version to get strings n and m

--- two_strings/two_strings.py
+++ two_strings/two_strings.py
@@ -17,7 +17,57 @@ Example:
 
 
 def two_string(n, m):
-    pass
+    n_list = []
+    for i in range(len(n)):
+        if i == 0:
+            if n[i] == '#':
+                continue
+            else:
+                n_list.append(n[i])
+        else:
+            if n[i] == '#':
+                if len(n_list) >= 1:
+                    n_list.pop()
+            else:
+                n_list.append(n[i])
+    new_string_n = ''
+    new_string_n = new_string_n.join(n_list)
+
+    m_list = []
+    for i in range(len(m)):
+        if i == 0:
+            if m[i] == '#':
+                continue
+            else:
+                m_list.append(m[i])
+        else:
+            if m[i] == '#':
+                if len(m_list) >= 1:
+                    m_list.pop()
+            else:
+                m_list.append(m[i])
+    new_string_m = ''
+    new_string_m = new_string_m.join(m_list)
+
+    if new_string_n == new_string_m:
+        return True
+    else:
+        return False
+
+
+'''
+Example "xy##":
+
+if 1 is # ignore
+if 1 is not # add it to the list
+if 2 is # and 1 was not # then remove 1 from the list
+if 2 is not # then add it to the list
+if 3 is # and there is a previous item in the list then remove it
+if 3 is not # then add it to the list
+if 4 is # and there is a previous item in the list then remove it
+if 4 is not # then add it to the list
+
+'''
 
 
 def test_two_string_a():
@@ -26,3 +76,11 @@ def test_two_string_a():
 
 def test_two_string_b():
     assert two_string('xy##', 'z#w#') is True
+
+
+def test_two_string_c():
+    assert two_string('xy##', 'zcw#') is False
+
+
+def test_two_string_d():
+    assert two_string('xy##oiuy##ijgs##fd#', 'oiiuy###i#ijgs###jf') is True
