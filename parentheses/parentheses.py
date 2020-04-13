3479966736d518d7ad8a7ae8919d2183d54c4de6
jenjnif <jenjnif@hotmail.com>
2020-04-13 09:35:46 +0100

    create file for generate parentheses challenge

---
+++ parentheses/parentheses.py
@@ -0,0 +1,24 @@
+'''
+13 April 2020
+This week’s question:
+Given a number n, write a function to generate all combinations
+of well-formed parentheses.
+
+Example:
+
+generateParens(3)
+[“((()))”,
+ “(()())”,
+ “(())()”,
+ “()(())”,
+ “()()()”
+]
+'''
+
+
+def generate_parentheses():
+    pass
+
+
+def test_generate_parentheses():
+    pass
