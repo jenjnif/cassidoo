b08b72a2ed64ca127b3c21d3a23fd4debc66d33c
jenjnif <jenjnif@hotmail.com>
2020-04-13 15:08:47 +0100

    add second failing test for type

--- parentheses/parentheses.py
+++ parentheses/parentheses.py
@@ -20,6 +20,10 @@ def generate_parentheses(n):
     pass
 
 
+def test_type_generate_parentheses():
+    assert type(generate_parentheses(3)) == list
+
+
 def test_generate_parentheses():
     assert generate_parentheses(3) == ["((()))",
                                        "(()())",
